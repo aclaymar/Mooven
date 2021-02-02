37ca48938fe599ed750c9a29e38628709f6950b2
Anderson Martins <54605764+aclaymar@users.noreply.github.com>
2021-02-02 09:31:25 -0300

    Buscando Repositório do Git hub

---
+++ Teste_Mooven/Teste_em_JS_Vanilla_Repositório/mani.js
@@ -0,0 +1,82 @@
+(function() {
+  const search = document.getElementById("search");
+  const profile = document.getElementById("profile");
+  const url = "https://api.github.com/users";
+  const client_id = "Iv1.58338dd36f6c46c8";
+  const client_secret = "bf3ac4b66cfc4bc9084f592735967475ffe6c7a3";
+  const count = 7;
+  const sort = "created: asc";
+
+
+  async function getUser(user) {
+    const profileResponse = await fetch(`${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+    
+    const reposResponse = await fetch(`${url}/${user}/respos?per_page=${count}&sort=${sort}&client_id=${client_id}&client_secret=${client_secret}`);
+    
+    const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
+
+
+    return { profile, repos };
+  }
+
+  function showProfile(user) {
+    profile.innerHTML = `<div class="row mt-3">
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="${user.avatar_url}" />
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Repositorios: <span class="badge badge-success">${user.public_repos}</span></li>
+            <li class="list-group-item">Seguidores: <span class="badge badge-primary">${user.followers}</span></li>
+            <li class="list-group-item">Seguindo: <span class="badge badge-info">${user.following}</span></li>
+          </ul>
+          <div class="card-body">
+            <a href="${user.htm_url}" target="_blank" class="btn btn-warning btn-block">Ver Perfil</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-8">
+        <div id="repos"></div>
+      </div>
+      </div>`;
+  }
+
+  function showRepos(repos){
+    let output = "";
+
+    repos.forEach(repo => {
+      output += `<div class="card card-body mb-2">
+      <div class="row">
+        <div class="col-md-6">
+          <a href="${repo.html_url}" target="black">${repo.name}</a>
+        </div>
+        <div class="col-md-6">
+          <span class="badge badge-primary">starts: ${
+            repo.stargazers_count
+          }</span>
+          <span class="badge badge-success">watch: ${
+            repo.watchers_count
+          }</span>
+          <span class="badge badge-warning">forks: ${
+            repo.forks_count
+          }</span>
+        </div>
+      </div>
+    </div>`;
+    });
+
+    document.getElementById("repos").innerHTML = output;
+  }
+
+
+  search.addEventListener("keyup", e => {
+    const user = e.target.value;
+
+    if (user.length > 0) {
+      getUser(user).then(res => {
+        showProfile(res.profile);
+        showRepos(res.repos);
+      });
+    }
+  });
+})();
